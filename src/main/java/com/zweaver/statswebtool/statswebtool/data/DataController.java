f3b6a2a6561cab0e6cecce51305f1d6a1229a022
Zach Weaver <49159969+Kiyoshika@users.noreply.github.com>
2021-02-15 21:20:13 -0500

    Update DataController.java

--- src/main/java/com/zweaver/statswebtool/statswebtool/data/DataController.java
+++ src/main/java/com/zweaver/statswebtool/statswebtool/data/DataController.java
@@ -23,7 +23,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 @RestController
-@CrossOrigin(origins = {"http://localhost:8080", "https://statisticswebtool.netlify.app/"})
+@CrossOrigin(origins = {"http://localhost:8080", "https://statisticswebtool.netlify.app", "https://statisticswebtool.netlify.app/"})
 public class DataController {
     // file name -- data object
     private HashMap<String, Data> dataStore = new HashMap<>();
